61728f0b598b8c18a4699caa731ca7182854c7ff
LMV-STUDIO <matiasvitaleimm@gmail.com>
2026-08-22 14:53:06 -0300

    Eliminar admin-fijos.js temporalmente

--- admin-fijos.js
+++
@@ -1,4 +0,0 @@
-// Archivo desactivado temporalmente.
-// Se mantiene para que el panel de administración que todavía lo referencia
-// no tenga errores de recurso mientras dejamos la versión estable.
-(() => {})();
